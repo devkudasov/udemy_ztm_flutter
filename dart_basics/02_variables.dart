153ee91b6fe9fc8bc6e6af1b0d73ae8c7568cb6b
Dmitriy Kudasov <dev.kudasov@gmail.com>
2023-03-26 22:15:51 +0300

    variables lecture

---
+++ dart_basics/02_variables.dart
@@ -0,0 +1,8 @@
+void main() {
+  int outputInt = 2; // initialization
+  print('outputInt: $outputInt');
+
+  late int lateInt; // declaration
+  lateInt = 42; // allocation
+  print('lateInt: $lateInt');
+}
