d5d52b60d489571d682182773ea78dd2b982c3ac
Dmitriy Kudasov <dev.kudasov@gmail.com>
2023-03-28 14:00:53 +0300

    dart: if conditions

---
+++ dart_basics/06_conditions.dart
@@ -0,0 +1,13 @@
+void main() {
+  int age = 19;
+
+  print('User is $age years old.');
+
+  if (age >= 18) {
+    print('You can enter the movie.');
+  } else if (age == 16) {
+    print('You can enter the movie with your parants.');
+  } else {
+    print('You can not enter the movie.');
+  }
+}
