61e7559b97473c6e53a4dede4ae2381b69b8423a
Dmitriy Kudasov <dev.kudasov@gmail.com>
2023-04-07 23:04:40 +0300

    flutter: Flex, Expanded

--- flutter_basics/lib/main.dart
+++ flutter_basics/lib/main.dart
@@ -44,6 +44,35 @@ class ScaffoldBody extends StatelessWidget {
         crossAxisAlignment: CrossAxisAlignment.end,
         mainAxisAlignment: MainAxisAlignment.spaceAround,
         children: [
+          Row(
+            mainAxisAlignment: MainAxisAlignment.end,
+            crossAxisAlignment: CrossAxisAlignment.center,
+            children: [
+              const Flexible(
+                child: Text(
+                  'I\'m the best flutter developer ; But this text doesn\'t feet whole screen width ;))',
+                  maxLines: 1,
+                  overflow: TextOverflow.ellipsis,
+                ),
+              ),
+              Container(
+                height: 20.0,
+                width: 20.0,
+                color: Colors.amber,
+              ),
+              const SizedBox(
+                width: 8.0,
+              ),
+              Container(
+                height: 20.0,
+                width: 20.0,
+                color: Colors.green,
+              ),
+              const SizedBox(
+                width: 8.0,
+              ),
+            ],
+          ),
           Container(
             color: Colors.green,
             child: const Padding(
