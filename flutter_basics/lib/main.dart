c80a3d0ee3d03ea36d3ab37d72451f2e4d940df0
Dmitriy Kudasov <dev.kudasov@gmail.com>
2023-04-10 20:53:29 +0300

    flutter: SingleChildScrollView

--- flutter_basics/lib/main.dart
+++ flutter_basics/lib/main.dart
@@ -38,98 +38,149 @@ class ScaffoldBody extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return Container(
-      color: Colors.pink,
-      child: Column(
-        mainAxisSize: MainAxisSize.max,
-        crossAxisAlignment: CrossAxisAlignment.end,
-        mainAxisAlignment: MainAxisAlignment.spaceAround,
-        children: [
-          Row(
-            mainAxisAlignment: MainAxisAlignment.end,
-            crossAxisAlignment: CrossAxisAlignment.center,
-            children: [
-              const Flexible(
+      color: Colors.white,
+      child: SingleChildScrollView(
+        physics: const BouncingScrollPhysics(),
+        child: Column(
+          mainAxisSize: MainAxisSize.max,
+          crossAxisAlignment: CrossAxisAlignment.center,
+          children: [
+            Row(
+              mainAxisAlignment: MainAxisAlignment.end,
+              crossAxisAlignment: CrossAxisAlignment.center,
+              children: [
+                const Flexible(
+                  child: Text(
+                    'I\'m the best flutter developer ; But this text doesn\'t feet whole screen width ;))',
+                    maxLines: 1,
+                    overflow: TextOverflow.ellipsis,
+                  ),
+                ),
+                Container(
+                  height: 20.0,
+                  width: 20.0,
+                  color: Colors.amber,
+                ),
+                const SizedBox(
+                  width: 8.0,
+                ),
+                Container(
+                  height: 20.0,
+                  width: 20.0,
+                  color: Colors.green,
+                ),
+                const SizedBox(
+                  width: 8.0,
+                ),
+              ],
+            ),
+            const SizedBox(
+              height: 20.0,
+            ),
+            Container(
+              color: Colors.green,
+              child: const Padding(
+                padding: EdgeInsets.all(20.0),
                 child: Text(
-                  'I\'m the best flutter developer ; But this text doesn\'t feet whole screen width ;))',
-                  maxLines: 1,
-                  overflow: TextOverflow.ellipsis,
+                  'First column child',
                 ),
               ),
-              Container(
-                height: 20.0,
-                width: 20.0,
-                color: Colors.amber,
-              ),
-              const SizedBox(
-                width: 8.0,
-              ),
-              Container(
-                height: 20.0,
-                width: 20.0,
-                color: Colors.green,
-              ),
-              const SizedBox(
-                width: 8.0,
-              ),
-            ],
-          ),
-          Container(
-            color: Colors.green,
-            child: const Padding(
-              padding: EdgeInsets.all(20.0),
-              child: Text(
-                'First column child',
-              ),
             ),
-          ),
-          const SizedBox(
-            height: 20.0,
-          ),
-          Container(
-            height: 200.0,
-            width: 200.0,
-            decoration: BoxDecoration(
-              borderRadius: BorderRadius.circular(10.0),
-              color: Colors.blue,
+            const SizedBox(
+              height: 20.0,
             ),
-            child: const Text(
-              'Hello World',
-              style: TextStyle(
-                color: Colors.red,
-                fontSize: 20.0,
+            Container(
+              height: 200.0,
+              width: 200.0,
+              decoration: BoxDecoration(
+                borderRadius: BorderRadius.circular(10.0),
+                color: Colors.blue,
               ),
-            ),
-          ),
-          Stack(
-            children: [
-              SizedBox(
-                height: 100.0,
-                width: 100.0,
-                child: Image.asset(
-                  'assets/asset.png',
-                  fit: BoxFit.cover,
+              child: const Text(
+                'Hello World',
+                style: TextStyle(
+                  color: Colors.red,
+                  fontSize: 20.0,
                 ),
               ),
-              const Positioned(
-                top: 45,
-                child: Text('Kudasov'),
-              ),
-            ],
-          ),
-          const SizedBox(
-            height: 100.0,
-            width: 100.0,
-            child: CircleAvatar(
-              backgroundImage: AssetImage('assets/asset.png'),
             ),
-          ),
-          const SizedBox(
-            height: 40.0,
-          ),
-          const Text(
-            'Last Column child',
-          ),
-        ],
+            const SizedBox(
+              height: 20.0,
+            ),
+            Stack(
+              children: [
+                SizedBox(
+                  height: 100.0,
+                  width: 100.0,
+                  child: Image.asset(
+                    'assets/asset.png',
+                    fit: BoxFit.cover,
+                  ),
+                ),
+                const Positioned(
+                  top: 45,
+                  child: Text('Kudasov'),
+                ),
+              ],
+            ),
+            const SizedBox(
+              height: 20.0,
+            ),
+            const SizedBox(
+              height: 100.0,
+              width: 100.0,
+              child: CircleAvatar(
+                backgroundImage: AssetImage('assets/asset.png'),
+              ),
+            ),
+            const SizedBox(
+              height: 40.0,
+            ),
+            const SizedBox(
+              height: 100.0,
+              width: 100.0,
+              child: CircleAvatar(
+                backgroundImage: AssetImage('assets/asset.png'),
+              ),
+            ),
+            const SizedBox(
+              height: 40.0,
+            ),
+            const SizedBox(
+              height: 100.0,
+              width: 100.0,
+              child: CircleAvatar(
+                backgroundImage: AssetImage('assets/asset.png'),
+              ),
+            ),
+            const SizedBox(
+              height: 40.0,
+            ),
+            const SizedBox(
+              height: 100.0,
+              width: 100.0,
+              child: CircleAvatar(
+                backgroundImage: AssetImage('assets/asset.png'),
+              ),
+            ),
+            const SizedBox(
+              height: 40.0,
+            ),
+            const SizedBox(
+              height: 100.0,
+              width: 100.0,
+              child: CircleAvatar(
+                backgroundImage: AssetImage('assets/asset.png'),
+              ),
+            ),
+            const SizedBox(
+              height: 40.0,
+            ),
+            const Text(
+              'Last Column child',
+            ),
+          ],
+        ),
       ),
     );
   }
